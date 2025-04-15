58c6484594bbcf512d68066c9c0c8b4675f12d42
Warda <wardaboubaker@outlook.com>
2025-04-15 18:08:54 +0100

    Ajout d'un gestionnaire d'erreurs global

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 const { login } = require('./auth/login');
 module.exports.auth = { login };
+// Gestionnaire d'erreurs global
+function handleError(error) {
+    console.error('Error:', error.message);
+}
+module.exports.handleError = handleError;
