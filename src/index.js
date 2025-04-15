24849a18217b4ca1711f1c43d8906ae5e5b31df7
Warda <wardaboubaker@outlook.com>
2025-04-15 21:35:36 +0100

    Ajout de la fonctionnalité feature/database-integration

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 const { login } = require('./auth/login');
 module.exports.auth = { login };
+function connect(){
+    //Logique de connexion à la base de données
+
+return { status: 'connected' };
+}
+module.exports = { connect };
