c5c568cba17a8536b6aa0ad0c47995602a8fba09
Warda <wardaboubaker@outlook.com>
2025-04-15 18:11:34 +0100

    Ajout d'une fonction utilitaire de formatage de date

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 const { login } = require('./auth/login');
 module.exports.auth = { login };
+// Utilitaires
+function formatDate(date) {
+    return date.toISOString();
+}
+module.exports.utils = { formatDate };
